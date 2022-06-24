b015482d7816290abb6bc704941abaac0880a37b
Kacper Kruger <kacper.kruger1@gmail.com>
2022-06-24 18:36:54 +0200

    added importing config from file

---
+++ src/main/scala/reader/ConfigReader.scala
@@ -0,0 +1,17 @@
+package reader
+
+import io.circe.generic.JsonCodec
+import io.circe.parser.parse
+
+object ConfigReader {
+  @JsonCodec case class Config(inputDirectory: String, outputDirectory: String)
+
+  def readConfig: Option[Config] = {
+    val configString = scala.io.Source.fromResource("config.json").mkString
+    val parsedConfig = parse(configString).toOption
+    for {
+      configJson <- parsedConfig
+      config <- configJson.as[Config].toOption
+    } yield config
+  }
+}
