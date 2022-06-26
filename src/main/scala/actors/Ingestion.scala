abfd6e60a3821959c99fedb498043c770cd0d9ef
Kacper Kruger <kacper.kruger1@gmail.com>
2022-06-27 00:43:45 +0200

    read and write functions

--- src/main/scala/actors/Ingestion.scala
+++ src/main/scala/actors/Ingestion.scala
@@ -23,3 +23,29 @@ object Ingestion {
     new Ingestion(inDir, outDir, cutOff, amountOfWorkers)
   )
 }
+
+trait IngestionHandler {
+  def getListOfImages(dirPath: String): List[File] = {
+    val dir = new File(dirPath)
+    dir.listFiles.filter(_.isFile).toList
+  }
+
+  def writeNewFile(
+      name: String,
+      label: LuminanceLabel,
+      score: Int,
+      inDir: String,
+      outDir: String
+  ): Unit = {
+    val splitName = name.split('.')
+    val image =
+      ImageIO.read(new File(s"${inDir}/${splitName.head}.${splitName.last}"))
+    ImageIO.write(
+      image,
+      splitName.last,
+      new File(
+        s"${outDir}/${splitName.head}_${label.value}_$score.${splitName.last}"
+      )
+    )
+  }
+}
