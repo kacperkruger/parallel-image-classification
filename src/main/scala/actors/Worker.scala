eecc92566af3a2bd22f5247d96a2d9587d1a655c
Kacper Kruger <kacper.kruger1@gmail.com>
2022-06-27 00:26:47 +0200

    improved worker logic and new lumi formula

--- src/main/scala/actors/Worker.scala
+++ src/main/scala/actors/Worker.scala
@@ -4,27 +4,43 @@ import actors.Worker.{Execute, Luminance, Result}
 import akka.actor.Actor
 
 import java.awt.Color
+import java.awt.image.BufferedImage
+import scala.math.sqrt
 
 object Worker {
-  case class Execute(id: Int, task: Array[Int])
-  case class Result(id: Int, luminance: Luminance)
-  case class Luminance(score: Double)
+  case class Execute(name: String, task: BufferedImage)
+  case class Result(name: String, luminance: Luminance)
+  case class Luminance(score: Int)
 }
 
 trait WorkerHandler {
-  def calculateLuminance(arr: Array[Int]): Luminance = {
-    val sumOfRGB = arr.foldLeft((0, 0, 0))((acc, pixel) => {
+  def convertIMGtoRGB(img: BufferedImage): Array[Int] = {
+    val w = img.getWidth()
+    val h = img.getHeight()
+    img.getRGB(0, 0, w, h, null, 0, w)
+  }
+  def calculateLuminance(image: Array[Int]): Luminance = {
+    val sumOfRGB = image.foldLeft((0, 0, 0))((acc, pixel) => {
       val c = new Color(pixel)
-      (acc._1 + c.getRed, acc._2 + c.getGreen, acc._3 + c.getBlue)
+      (
+        acc._1 + c.getRed,
+        acc._2 + c.getGreen,
+        acc._3 + c.getBlue
+      )
     })
+    val R = (sumOfRGB._1.toDouble / image.length) / 255
+    val G = (sumOfRGB._2.toDouble / image.length) / 255
+    val B = (sumOfRGB._3.toDouble / image.length) / 255
     Luminance(
-      0.2126 * sumOfRGB._1 + 0.75152 * sumOfRGB._2 + 0.0722 * sumOfRGB._3
+      (sqrt(
+        0.299 * (R * R) + 0.587 * (G * G) + 0.114 * (B * B)
+      ) * 100).toInt
     )
   }
 }
 
 class Worker extends Actor with WorkerHandler {
-  override def receive: Receive = { case Execute(id, task) =>
-    sender() ! Result(id, calculateLuminance(task))
+  override def receive: Receive = { case Execute(name, task) =>
+    sender() ! Result(name, calculateLuminance(convertIMGtoRGB(task)))
   }
 }
